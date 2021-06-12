2e3225a21d4e46efe39f36aa16ea34016ec689dd
StanislavSamokhvalov <stanislavsamokhvalov7@gmail.com>
2021-06-12 19:44:58 +0400

    initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+const val PERCENT = 0.0075
+const val MIN_COMISSION = 3500
+
+fun main() {
+    print("Enter the transfer amount (in kopek): ")
+    val amount = readLine()!!.toInt()
+    val step = MIN_COMISSION/PERCENT
+    val commission = if (amount <= step) MIN_COMISSION else amount*PERCENT
+    print("The transfer amount will be (in kopek): $commission")
+}
