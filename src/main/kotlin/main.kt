5defcdd2084993a797281a018db513462d3ac2d0
StanislavSamokhvalov <stanislavsamokhvalov7@gmail.com>
2021-06-14 15:15:58 +0400

    update

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,7 +4,7 @@ const val MIN_COMISSION = 3500
 fun main() {
     print("Enter the transfer amount (in kopek): ")
     val amount = readLine()!!.toInt()
-    val step = MIN_COMISSION / PERCENT
-    val commission = if (amount <= step) MIN_COMISSION else amount * PERCENT
-    print("The transfer amount will be (in kopek): $commission")
+    val commission = amount * PERCENT
+    val finalCommission = if (commission <= MIN_COMISSION) MIN_COMISSION else commission
+    print("The transfer amount will be (in kopek): $finalCommission")
 }
